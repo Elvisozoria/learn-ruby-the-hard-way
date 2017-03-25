1cab8ae4cb8f2a5c1c2708f4df8725fc2c57dbc0
Elvis <elvis.ozoria@gmail.com>
2017-03-25 16:34:34 -0400

    from 11 until half of 15
    I typed 15 fully but it gives me an error. I just discovered that the
    book on hte website gives me a diferet code that the one that I have...
    maybe the website one is more updated?... I will try with that one
    tomorw

---
+++ ex11.rb
@@ -0,0 +1,9 @@
+print "How old are you?"
+age.chomp() = gets()
+print "How tall are you?"
+height = gets.chomp()
+print  "How much do you weight?" #there was a spelling mistake here on the book, it said "How much do you you weigh?"
+weight = gets.chomp()
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
