bbe4141a5a31033e3179c9c210ca11dd93bd3253
Elvis <elvis.ozoria@gmail.com>
2017-03-26 20:26:17 -0400

    finaly understanding this exercice
    i will do all the exercices from the webbook, the other oneas are
    updated...

--- ex15.rb
+++ ex15.rb
@@ -1,15 +1,18 @@
+#Gets the name of the sample file as an argument and save it on a variable
 filename = ARGV.first
 
 prompt= "-->"
-txt = File.open(filename)
+txt = open(filename)
 
 puts "Here's your file: #{filename}"
+#reads the file we just opened
 puts txt.read()
 
-puts "I'll also ask you to type it again:"
+puts "I'll also ask you to type the name again:"
 print prompt
-file_again = STDIN.gets.chomp()
+#the user input the name of the file for no reason, or he could also enter the name of other file
+file_again = $stdin.gets.chomp
 
-txt_again = File.open(file_again)
-
-puts txt_again.read()
+txt_again = open(file_again)
+#this open the file that the user just wrote, so instead of doing it from ARGSV this is the way to doit form a prompt
+puts txt_again.read
