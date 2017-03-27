96607bb8305494208fb1095b6ca7f444f0faf3b8
Elvis <elvis.ozoria@gmail.com>
2017-03-27 15:02:44 -0400

    more experiments with ex15

--- ex15.rb
+++ ex15.rb
@@ -5,8 +5,8 @@ prompt= "-->"
 txt = open(filename)
 
 puts "Here's your file: #{filename}"
-#reads the file we just opened
-puts txt.read()
+puts txt.read #reads the file we just opened
+txt.close #Closing the file
 
 puts "I'll also ask you to type the name again:"
 print prompt
@@ -16,3 +16,7 @@ file_again = $stdin.gets.chomp
 txt_again = open(file_again)
 #this open the file that the user just wrote, so instead of doing it from ARGSV this is the way to doit form a prompt
 puts txt_again.read
+
+txt_again.close  #Closing the file
+
+#looks like calling a function without arguments with or without parentesis "()"  works equaly well
